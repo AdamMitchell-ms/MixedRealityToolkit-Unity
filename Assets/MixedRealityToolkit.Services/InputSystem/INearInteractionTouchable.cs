98ffb02008c66cc5abd2317cd4c028c633cd2fbf
Adam Mitchell <adammit@microsoft.com>
2019-08-22 14:32:23 -0700

    initial rough working

--- Assets/MixedRealityToolkit.Services/InputSystem/INearInteractionTouchable.cs
+++ Assets/MixedRealityToolkit.Services/InputSystem/INearInteractionTouchable.cs
@@ -7,6 +7,7 @@ using UnityEngine;
 
 namespace Microsoft.MixedReality.Toolkit.Input
 {
+    //TODO: can all of this go in BaseNearInteractionTouchable
     public interface INearInteractionTouchable
     {
         Vector3 LocalCenter { get; set; }
