1b2fd6a76d64cf65aeffdef6602230df4a424e98
Adam Mitchell <adammit@microsoft.com>
2019-08-23 16:32:26 -0700

    Clean up the sample scene and remove temp stuff

--- Assets/MixedRealityToolkit.SDK/Features/UX/Scripts/Utilities/RectTransformCubeScaler.cs
+++ Assets/MixedRealityToolkit.SDK/Features/UX/Scripts/Utilities/RectTransformCubeScaler.cs
@@ -8,36 +8,43 @@ public class RectTransformCubeScaler : MonoBehaviour
 {
     [SerializeField]
     [Range(0.0f, 1.0f)]
+    [Tooltip("0 means set localScale.z to Min(x, y).  1 means set localScale z to Max(x, y)")]
     private float minOrMaxLerpFactor = 0.0f;
 
     [SerializeField]
     [Range(0.0f, 1.0f)]
+    [Tooltip("0 means set localScale.z to x.  1 means set localScale z to y")]
     private float xOrYLerpFactor = 0.0f;
 
     [SerializeField]
     [Range(0.0f, 1.0f)]
     private float minMaxOrXYLerpFactor = 0.0f;
+    [Tooltip("0 means set localScale.z to value derived from min/max lerp.  1 means set localScale.z to value derived from x/y lerp")]
 
     private RectTransform rectTransform;
+    private Vector2 prevRectSize = default;
 
-    // Start is called before the first frame update
-    void Start()
+    private void Start()
     {
         rectTransform = GetComponent<RectTransform>();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
         var size = rectTransform.rect.size;
 
-        var min = Mathf.Min(size.x, size.y);
-        var max = Mathf.Max(size.x, size.y);
-        var minOrMax = Mathf.Lerp(min, max, minOrMaxLerpFactor);
-        var xOrY = Mathf.Lerp(size.x, size.y, xOrYLerpFactor);
+        if (prevRectSize != size)
+        {
+            prevRectSize = size;
 
-        var z = Mathf.Lerp(minOrMax, xOrY, minMaxOrXYLerpFactor);
+            var min = Mathf.Min(size.x, size.y);
+            var max = Mathf.Max(size.x, size.y);
+            var minOrMax = Mathf.Lerp(min, max, minOrMaxLerpFactor);
+            var xOrY = Mathf.Lerp(size.x, size.y, xOrYLerpFactor);
 
-        this.transform.localScale = new Vector3(size.x, size.y, z);
+            var z = Mathf.Lerp(minOrMax, xOrY, minMaxOrXYLerpFactor);
+
+            this.transform.localScale = new Vector3(size.x, size.y, z);
+        }
     }
 }
