fb8041d3d7672cd9b47a2cae99907ef51f688c80
Adam Mitchell <adammit@microsoft.com>
2019-08-23 16:01:06 -0700

    Get compressable pressablebutton working on UnityUI.  Some issues with scaling around the wireframe borders.

---
+++ Assets/MixedRealityToolkit.SDK/Features/UX/Scripts/Utilities/RectTransformCubeScaler.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+[RequireComponent(typeof(RectTransform))]
+public class RectTransformCubeScaler : MonoBehaviour
+{
+    [SerializeField]
+    [Range(0.0f, 1.0f)]
+    private float minOrMaxLerpFactor = 0.0f;
+
+    [SerializeField]
+    [Range(0.0f, 1.0f)]
+    private float xOrYLerpFactor = 0.0f;
+
+    [SerializeField]
+    [Range(0.0f, 1.0f)]
+    private float minMaxOrXYLerpFactor = 0.0f;
+
+    private RectTransform rectTransform;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rectTransform = GetComponent<RectTransform>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        var size = rectTransform.rect.size;
+
+        var min = Mathf.Min(size.x, size.y);
+        var max = Mathf.Max(size.x, size.y);
+        var minOrMax = Mathf.Lerp(min, max, minOrMaxLerpFactor);
+        var xOrY = Mathf.Lerp(size.x, size.y, xOrYLerpFactor);
+
+        var z = Mathf.Lerp(minOrMax, xOrY, minMaxOrXYLerpFactor);
+
+        this.transform.localScale = new Vector3(size.x, size.y, z);
+    }
+}
